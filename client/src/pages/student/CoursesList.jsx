a173197d8873eeac800b10f52c8741fd2a1c0e7c
geetha8888 <geethamurthy8888@gmail.com>
2026-08-15 17:57:36 +0530

    removedfromstudents

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CoursesList = () => {
   return (
-    <div>Im from CoursesList page for students in the student folder</div>
+    <div>Im from CoursesList page</div>
   )
 }
 
